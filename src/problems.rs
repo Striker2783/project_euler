416acada2471a1cae8e8e9ca3e08b6a89fdca4f8
Striker2783 <owenxingli@gmail.com>
2023-05-16 19:58:15 -0400

    init

---
+++ src/problems.rs
@@ -0,0 +1,55 @@
+#![allow(dead_code, unused)]
+#[path = "../src/file_parsers.rs"]
+mod file_parsers;
+#[path = "../src/utilities.rs"]
+mod utilities;
+
+pub fn one() {
+    const MAX: i32 = 1000;
+    let sum: i32 = (0..)
+        .take_while(|x| x < &MAX)
+        .filter(|x| x % 5 == 0 || x % 3 == 0)
+        .sum();
+    println!("{sum}")
+}
+pub fn two() {
+    const MAX: u64 = 4_000_000;
+    let test = utilities::Fibonacci::new();
+    let sum: u64 = test
+        .into_iter()
+        .take_while(|x| x < &MAX)
+        .filter(|x| x % 2 == 0)
+        .sum();
+    println!("{sum}")
+}
+pub fn three() {
+    const NUM: u64 = 600851475143;
+    let mut largest = 0;
+    let mut n = NUM;
+    let mut i = 2;
+    while n > 1 {
+        if n % i == 0 {
+            n /= i;
+            if i > largest {
+                largest = i;
+            }
+            i = 2;
+            continue;
+        }
+        i += 1;
+    }
+    println!("{largest}");
+}
+pub fn four() {
+    const MIN: u64 = 100;
+    const MAX: u64 = 1000;
+    let map1 = MIN..MAX;
+    let largest = map1
+        .flat_map(|i| (i..MAX).map(move |j| i * j))
+        .filter(|x| utilities::is_palindromic(x))
+        .max();
+    if let Some(a) = largest {
+        println!("{}", a);
+    }
+}
+pub fn curr() {}
---
+++ src/utilities.rs
@@ -0,0 +1,31 @@
+pub struct Fibonacci {
+    next: u64,
+    curr: u64,
+}
+
+impl Fibonacci {
+    pub fn new() -> Self {
+        return Fibonacci { next: 1, curr: 0 };
+    }
+}
+
+impl Iterator for Fibonacci {
+    type Item = u64;
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_next = self.curr.checked_add(self.next);
+
+        if let Some(n) = new_next {
+            self.curr = self.next;
+            self.next = n;
+
+            return Some(self.curr);
+        }
+        return None;
+    }
+}
+
+pub fn is_palindromic(x: &u64) -> bool {
+    let reverse: Vec<char> = x.to_string().chars().rev().collect();
+    let curr: Vec<char> = x.to_string().chars().collect();
+    return reverse == curr;
+}
