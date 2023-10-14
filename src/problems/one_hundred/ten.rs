7d6516302136d7cecfd4b197d58832e7e1ecf9f3
Striker2783 <owenxingli@gmail.com>
2023-10-14 18:48:55 -0400

    Twenty Seven

--- src/problems/one_hundred/ten.rs
+++ src/problems/one_hundred/ten.rs
@@ -1,13 +1,15 @@
+use std::collections::HashSet;
+
 pub fn ten() {
-    let a = sieve_of_eratosthenes(&2_000_000);
+    let a = sieve_of_eratosthenes(2_000_000);
     let sum: u64 = a.iter().sum();
     println!("{sum}");
 }
-pub fn sieve_of_eratosthenes(upper: &u64) -> Vec<u64> {
-    let range = 0u64..*upper;
+pub fn sieve_of_eratosthenes(upper: u64) -> Vec<u64> {
+    let range = 0u64..upper;
     let mut vec: Vec<bool> = range.map(|_| true).collect();
     for i in 2.. {
-        if i * i > *upper {
+        if i * i > upper {
             break;
         }
         if !vec[i as usize] {
@@ -19,10 +21,15 @@ pub fn sieve_of_eratosthenes(upper: &u64) -> Vec<u64> {
         }
     }
     let mut primes: Vec<u64> = vec![];
-    for i in 2..vec.len() {
+    (2..vec.len()).for_each(|i| {
         if vec[i] {
             primes.push(i as u64);
         }
-    }
+    });
     primes
+}
+
+pub fn sieve_of_eratosthenes_set(upper: u64) -> HashSet<u64> {
+    let primes = sieve_of_eratosthenes(upper);
+    primes.iter().copied().collect()
 }
