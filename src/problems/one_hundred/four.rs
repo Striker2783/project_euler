87c27be5007a9e9900f6e2175b74d28e371ae25a
Striker2783 <owenxingli@gmail.com>
2023-11-14 10:16:59 -0500

    Four Improvements

--- src/problems/one_hundred/four.rs
+++ src/problems/one_hundred/four.rs
@@ -4,7 +4,7 @@ pub fn four() {
     let map1 = MIN..MAX;
     let largest = map1
         .flat_map(|i| (i..MAX).map(move |j| i * j))
-        .filter(|x| is_palindromic(x))
+        .filter(is_palindromic)
         .max();
     if let Some(a) = largest {
         println!("{}", a);
@@ -14,5 +14,5 @@ pub fn four() {
 pub fn is_palindromic(x: &u64) -> bool {
     let reverse: Vec<char> = x.to_string().chars().rev().collect();
     let curr: Vec<char> = x.to_string().chars().collect();
-    return reverse == curr;
+    reverse == curr
 }
