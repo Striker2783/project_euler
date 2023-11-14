951caced6c1434f87a17f1fd541afb3b849337ab
Striker2783 <owenxingli@gmail.com>
2023-11-14 10:16:12 -0500

    Nine Improvements

--- src/problems/one_hundred/nine.rs
+++ src/problems/one_hundred/nine.rs
@@ -5,7 +5,7 @@ pub fn nine() {
         println!("{}", a.0 * a.1 * a.2);
     }
 }
-pub fn pythagoreon_triples<'a>(upper: &'a u64) -> impl Iterator<Item = (u64, u64, u64)> + 'a {
+pub fn pythagoreon_triples(upper: &u64) -> impl Iterator<Item = (u64, u64, u64)> + '_ {
     (1..*upper).flat_map(|x| {
         (1..*upper).filter_map(move |y| {
             let x_squared = x * x;
@@ -13,9 +13,9 @@ pub fn pythagoreon_triples<'a>(upper: &'a u64) -> impl Iterator<Item = (u64, u64
             let c_squared = x_squared + y_squared;
             let c = (c_squared as f64).sqrt() as u64;
             if c * c == c_squared {
-                return Some((x, y, c));
+                Some((x, y, c))
             } else {
-                return None;
+                None
             }
         })
     })
