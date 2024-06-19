62747cff7704a6f7dadf0a04365b22fef0ba92ae
Striker2783 <owenxingli@gmail.com>
2024-06-19 12:38:00 -0400

    39 Optimized (by 200x)

--- src/problems/one_hundred/thirty_nine.rs
+++ src/problems/one_hundred/thirty_nine.rs
@@ -1,38 +1,68 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::common::number_series::Squares;
+
 const MAX: u32 = 1000;
 pub fn run() {
-    println!("{}", solve(MAX));
+    let mut solver = Solver::new(MAX);
+    println!("{}", solver.solve());
 }
 
-fn solve(max: u32) -> u32 {
-    (3..=max)
-        .map(|x| (x, get_solutions(x)))
-        .max_by(|a, b| a.1.cmp(&b.1))
-        .unwrap()
-        .0
+struct Solver {
+    squares: HashMap<u32, u32>,
+    map: HashMap<u32, u32>,
+    max: u32,
 }
-
-fn get_solutions(p: u32) -> u32 {
-    (3..=(p / 3))
-        .map(|n| {
-            (n..=(n + p / 3))
-                .filter(|&m| {
-                    let c_squared = m * m + n * n;
-                    let c = (2..)
-                        .take_while(|&x| x * x <= c_squared)
-                        .find(|x| x * x == c_squared);
-                    c.is_some() && n + m + c.unwrap() == p
-                })
-                .count() as u32
-        })
-        .sum()
+impl Solver {
+    fn new(max: u32) -> Solver {
+        Self {
+            squares: Squares::default()
+                .enumerate()
+                .take_while(|(_, x)| *x < max * max)
+                .map(|(a, b)| (b, a as u32 + 1))
+                .collect(),
+            map: HashMap::new(),
+            max,
+        }
+    }
+    fn solve(mut self) -> u32 {
+        for a in 1..self.max {
+            for b in a..self.max {
+                self.insert_pair(a, b);
+            }
+        }
+        *self.map.iter().max_by(|a, b| a.1.cmp(b.1)).unwrap().0
+    }
+    fn insert_pair(&mut self, a: u32, b: u32) -> bool {
+        let c_s = a * a + b * b;
+        let c = match self.squares.get(&c_s) {
+            Some(a) => *a,
+            None => return false,
+        };
+        if c + a + b > self.max {
+            return false;
+        }
+        self.map
+            .entry(c + a + b)
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+        true
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::problems::one_hundred::thirty_nine::get_solutions;
+    use crate::thirty_nine::MAX;
+
+    use super::Solver;
 
     #[test]
-    fn t_get_solutions() {
-        assert_eq!(get_solutions(120), 3);
+    fn test_solver() {
+        let mut solver = Solver::new(120);
+        assert!(solver.insert_pair(20, 48));
+        solver = Solver::new(120);
+        assert_eq!(solver.solve(), 120);
+        solver = Solver::new(MAX);
+        assert_eq!(solver.solve(), 840);
     }
 }
