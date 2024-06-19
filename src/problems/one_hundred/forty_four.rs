ce90b129c3c1518a737a34e72517dcb4497833d7
Striker2783 <owenxingli@gmail.com>
2024-06-19 08:44:33 -0400

    Improved performance by 1.7x roughly

--- src/problems/one_hundred/forty_four.rs
+++ src/problems/one_hundred/forty_four.rs
@@ -1,19 +1,25 @@
 use std::{collections::HashSet, ops::Add};
 
+use num::integer::Roots;
+
 pub fn run() {
     let min = solve();
     println!("{min}");
 }
 
+fn is_pentagonal(n: u32) -> bool {
+    let x = (1 + (24 * n + 1).sqrt()) / 6;
+    x * (3 * x - 1) / 2 == n
+}
+
 fn solve() -> u32 {
-    let set: HashSet<_> = PentagonalNumbers::default().collect();
     let mut min = u32::MAX;
     for a in PentagonalNumbers::default() {
         for b in PentagonalNumbers::default() {
             if b.abs_diff(a) > min {
                 continue;
             }
-            if set.contains(&a.add(b)) && set.contains(&b.abs_diff(a)) {
+            if is_pentagonal(a.add(b)) && is_pentagonal(b.abs_diff(a)) {
                 min = b.abs_diff(a).min(min);
             }
         }
