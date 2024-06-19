6a94980f76d830d74c04d89dcd0a9519f0fdeeb5
Striker2783 <owenxingli@gmail.com>
2024-06-19 08:36:29 -0400

    Improved 44 performance 10x

--- src/problems/one_hundred/forty_four.rs
+++ src/problems/one_hundred/forty_four.rs
@@ -1,22 +1,24 @@
 use std::{collections::HashSet, ops::Add};
 
 pub fn run() {
-    let set: HashSet<_> = PentagonalNumbers::default().collect();
-    let min = PentagonalNumbers::default()
-        .filter_map(|a| {
-            PentagonalNumbers::default()
-                .filter_map(|b| {
-                    if set.contains(&a.add(b)) && set.contains(&b.abs_diff(a)) {
-                        Some(b.abs_diff(a))
-                    } else {
-                        None
-                    }
-                })
-                .min()
-        })
-        .min()
-        .unwrap();
+    let min = solve();
     println!("{min}");
+}
+
+fn solve() -> u32 {
+    let set: HashSet<_> = PentagonalNumbers::default().collect();
+    let mut min = u32::MAX;
+    for a in PentagonalNumbers::default() {
+        for b in PentagonalNumbers::default() {
+            if b.abs_diff(a) > min {
+                continue;
+            }
+            if set.contains(&a.add(b)) && set.contains(&b.abs_diff(a)) {
+                min = b.abs_diff(a).min(min);
+            }
+        }
+    }
+    min
 }
 
 #[derive(Debug, Default)]
@@ -28,11 +30,6 @@ impl Iterator for PentagonalNumbers {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.n += 1;
-        let checked = self.n.checked_mul(3 * self.n - 1);
-        if let Some(m) = checked {
-            Some(m / 2)
-        } else {
-            None
-        }
+        Some(self.n.checked_mul(3 * self.n - 1)? / 2)
     }
 }
