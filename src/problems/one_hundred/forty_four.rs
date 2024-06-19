170a726bf509138b032aa8a13c8e02f014e8d15e
Striker2783 <owenxingli@gmail.com>
2024-06-19 11:58:38 -0400

    44 Works and 20x faster.

--- src/problems/one_hundred/forty_four.rs
+++ src/problems/one_hundred/forty_four.rs
@@ -8,19 +8,38 @@ pub fn run() {
 }
 
 fn is_pentagonal(n: u32) -> bool {
-    let x = (1 + (24 * n + 1).sqrt()) / 6;
-    x * (3 * x - 1) / 2 == n
+    24u32
+        .checked_mul(n)
+        .and_then(|m| {
+            let x = (m + 1).sqrt() + 1;
+            let x = (x / 6);
+            if x * (3 * x - 1) / 2 == n {
+                Some(())
+            } else {
+                None
+            }
+        })
+        .is_some()
+}
+
+fn get_min_pent(p: u32, min: u32) -> u32 {
+    (1 + (p
+        .saturating_mul(8)
+        .saturating_add(min.saturating_mul(8))
+        .saturating_add(1))
+    .sqrt())
+        / 6
 }
 
 fn solve() -> u32 {
     let mut min = u32::MAX;
-    for a in PentagonalNumbers::default() {
-        for b in PentagonalNumbers::default() {
-            if b.abs_diff(a) > min {
-                continue;
-            }
-            if is_pentagonal(a.add(b)) && is_pentagonal(b.abs_diff(a)) {
-                min = b.abs_diff(a).min(min);
+    for (i, a) in PentagonalNumbers::default().enumerate() {
+        for b in PentagonalNumbers::default()
+            .skip(i + 1)
+            .take((get_min_pent(a, min) as usize).saturating_sub(i + 1))
+        {
+            if is_pentagonal(a + b) && is_pentagonal(b - a) {
+                min = (b - a).min(min);
             }
         }
     }
@@ -37,5 +56,20 @@ impl Iterator for PentagonalNumbers {
     fn next(&mut self) -> Option<Self::Item> {
         self.n += 1;
         Some(self.n.checked_mul(3 * self.n - 1)? / 2)
+    }
+
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        self.n += n as u32;
+        self.next()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::forty_four::solve;
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(solve(), 5482660)
     }
 }
