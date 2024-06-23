8994aa156db6845ce93e04a3880d71c8919db86a
Striker2783 <owenxingli@gmail.com>
2024-06-23 15:50:08 -0400

    54

--- src/problems/one_hundred/fifties/mod.rs
+++ src/problems/one_hundred/fifties/mod.rs
@@ -1,8 +1,15 @@
 use crate::macros::tens;
 
 pub mod fifty;
+pub mod fifty_four;
 pub mod fifty_one;
 pub mod fifty_three;
 pub mod fifty_two;
 
-tens!(&[fifty::run, fifty_one::run, fifty_two::run, fifty_three::run]);
+tens!(&[
+    fifty::run,
+    fifty_one::run,
+    fifty_two::run,
+    fifty_three::run,
+    fifty_four::run
+]);
