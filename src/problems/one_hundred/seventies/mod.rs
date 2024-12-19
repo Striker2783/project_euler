5c57eab973a90718713106e773186c43756fd23a
Striker2783 <owenxingli@gmail.com>
2024-12-19 17:31:29 -0500

    73

--- src/problems/one_hundred/seventies/mod.rs
+++ src/problems/one_hundred/seventies/mod.rs
@@ -2,4 +2,5 @@ use crate::macros::tens;
 mod seventy;
 mod seventy_one;
 mod seventy_two;
-tens!(&[seventy::run, seventy_one::run, seventy_two::run]);
+mod seventy_three;
+tens!(&[seventy::run, seventy_one::run, seventy_two::run, seventy_three::run]);
