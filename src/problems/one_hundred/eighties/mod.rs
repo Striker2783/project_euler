8627f028b5872b0eaab9b59bee2d409e3b13c51f
Striker2783 <owenxingli@gmail.com>
2024-12-20 22:14:37 -0500

    83

--- src/problems/one_hundred/eighties/mod.rs
+++ src/problems/one_hundred/eighties/mod.rs
@@ -1,7 +1,8 @@
 mod eighty;
 mod eighty_one;
 mod eighty_two;
+mod eighty_three;
 
 use crate::macros::tens;
 
-tens!(&[eighty::run, eighty_one::run, eighty_two::run]);
+tens!(&[eighty::run, eighty_one::run, eighty_two::run, eighty_three::run]);
