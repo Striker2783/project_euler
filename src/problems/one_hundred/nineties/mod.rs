8dd81b62d1af52ba07e7c1f1527104a35a41eaab
Striker2783 <owenxingli@gmail.com>
2025-01-13 16:28:03 -0500

    97

--- src/problems/one_hundred/nineties/mod.rs
+++ src/problems/one_hundred/nineties/mod.rs
@@ -1,11 +1,21 @@
 mod ninety;
-mod ninety_one;
-mod ninety_two;
-mod ninety_three;
-mod ninety_four;
 mod ninety_five;
+mod ninety_four;
+mod ninety_one;
+mod ninety_seven;
 mod ninety_six;
+mod ninety_three;
+mod ninety_two;
 
 use crate::macros::tens;
 
-tens!(&[ninety::run, ninety_one::run, ninety_two::run, ninety_three::run, ninety_four::run, ninety_five::run, ninety_six::run]);
+tens!(&[
+    ninety::run,
+    ninety_one::run,
+    ninety_two::run,
+    ninety_three::run,
+    ninety_four::run,
+    ninety_five::run,
+    ninety_six::run,
+    ninety_seven::run
+]);
---
+++ src/problems/one_hundred/nineties/ninety_seven.rs
@@ -0,0 +1,18 @@
+use num::traits::WrappingMul;
+
+pub fn run() {
+    println!("{}", solve());
+}
+fn solve() -> u128 {
+    let mut res = 28433;
+    let mut pow = 7_830_457;
+    let mut base = 2;
+    while pow > 0 {
+        if pow & 1 == 1 {
+            res = (res * base) % 10_000_000_000;
+        }
+        pow >>= 1;
+        base = (base * base) % 10_000_000_000;
+    }
+    (res + 1) % 10_000_000_000
+}
