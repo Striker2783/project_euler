8e69f5f6354c8000f6da2c557e24548c28a2428d
Striker2783 <owenxingli@gmail.com>
2025-01-12 22:41:23 -0500

    94 TODO

--- src/problems/one_hundred/nineties/mod.rs
+++ src/problems/one_hundred/nineties/mod.rs
@@ -2,7 +2,8 @@ mod ninety;
 mod ninety_one;
 mod ninety_two;
 mod ninety_three;
+mod ninety_four;
 
 use crate::macros::tens;
 
-tens!(&[ninety::run, ninety_one::run, ninety_two::run, ninety_three::run]);
+tens!(&[ninety::run, ninety_one::run, ninety_two::run, ninety_three::run, ninety_four::run]);
