a8a7dbf4868ea604827452531dc05d5ffdfc16c6
Striker2783 <owenxingli@gmail.com>
2025-11-28 23:26:14 -0500

    113

--- crates/two_hundred/src/tens/thirteen.rs
+++ crates/two_hundred/src/tens/thirteen.rs
@@ -1,17 +1,25 @@
 pub fn run() {
-    solve(1);
+    println!("{}", solve(100));
 }
 
 fn solve(pow: u32) -> u64 {
     let mut dp_dec = vec![vec![0; 10]; pow as usize + 1];
     dp_dec[0] = vec![1; 10];
     dp_dec[1] = vec![1; 10];
+    let mut dp_inc = vec![vec![0; 10]; pow as usize + 1];
+    dp_inc[0] = vec![1; 10];
+    dp_inc[1] = vec![1; 10];
     for i in 2..=(pow as usize) {
-        dp_dec[i][0] = 1;
-        for j in 1..=9 {
+        for j in 0..=9 {
             dp_dec[i][j] = 0;
             for k in 0..=j {
                 dp_dec[i][j] += dp_dec[i - 1][k];
+            }
+        }
+        for j in (1..=9).rev() {
+            dp_inc[i][j] = 0;
+            for k in j..=9 {
+                dp_inc[i][j] += dp_inc[i - 1][k];
             }
         }
     }
@@ -20,9 +28,12 @@ fn solve(pow: u32) -> u64 {
         .skip(1)
         .map(|v| v.iter().sum::<u64>())
         .sum::<u64>();
-    let mut dp_inc = vec![vec![0; 10]; pow as usize + 1];
-    
-    dec
+    let inc = dp_inc
+        .iter()
+        .skip(1)
+        .map(|v| v.iter().sum::<u64>())
+        .sum::<u64>();
+    dec + inc - 10 * pow as u64 - 1
 }
 
 #[cfg(test)]
@@ -32,7 +43,7 @@ mod tests {
     #[test]
     fn test_solve() {
         assert_eq!(solve(1), 9);
-        assert_eq!(solve(3), 99);
+        assert_eq!(solve(2), 99);
         assert_eq!(solve(6), 12951);
     }
 }
