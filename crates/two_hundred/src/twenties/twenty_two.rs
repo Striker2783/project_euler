c2a4899a112f30df54f39e697652fe657d562bfc
Striker2783 <owenxingli@gmail.com>
2025-11-29 17:42:09 -0500

    Optimized 122

--- crates/two_hundred/src/twenties/twenty_two.rs
+++ crates/two_hundred/src/twenties/twenty_two.rs
@@ -2,16 +2,6 @@ pub fn run() {
     println!("{}", solve(200));
 }
 
-fn binary(mut e: u32) -> u32 {
-    let original_e = e;
-    let mut a = 0;
-    while e > 0 {
-        e /= 2;
-        a += 1;
-    }
-    a + original_e.count_ones() - 2
-}
-
 fn solve(n: u32) -> u32 {
     (1..=n).map(mults).sum::<u32>()
 }
@@ -24,32 +14,30 @@ fn mults_helper(target: u32, curr_v: &mut Vec<u32>, mut min_chain: usize) -> usi
     } else if curr == target {
         return curr_v.len() - 1;
     }
-
-    for i in 0..curr_v.len() {
+    for i in (0..curr_v.len()).rev() {
         let value = curr_v[i];
         curr_v.push(curr + value);
         let res = mults_helper(target, curr_v, min_chain);
         curr_v.pop();
         min_chain = min_chain.min(res);
+
+        if curr_v.len() >= min_chain {
+            break;
+        }
     }
     min_chain
 }
 
 fn mults(e: u32) -> u32 {
-    mults_helper(e, &mut vec![1], 
-        binary(e) as usize
-    ) as u32
+    mults_helper(e, &mut vec![1], usize::MAX) as u32
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::twenties::twenty_two::{binary, mults};
+    use crate::twenties::twenty_two::mults;
 
     #[test]
     fn test_mults() {
-        assert_eq!(binary(15), 6);
-        assert_eq!(binary(16), 4);
-
         assert_eq!(mults(1), 0);
         assert_eq!(mults(2), 1);
         assert_eq!(mults(3), 2);
